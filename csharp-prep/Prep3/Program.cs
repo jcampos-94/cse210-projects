cd24b8cd81007232539dfb4e8b1f0953f879dfd4
jcampos-94 <jcampos.94@hotmail.com>
2023-01-09 15:34:09 -0500

    Completed Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string keepPlaying = "yes";
+
+        while (keepPlaying == "yes")
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+
+            int attempts = 0;
+
+            while (number != guess)
+            {
+                Console.Write("What is your guess? ");
+                string valueInText = Console.ReadLine();
+                guess = int.Parse(valueInText);
+
+                if (number < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                else if (number > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+
+                attempts += 1;
+            }
+
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"It took you {attempts} attempts!");
+                        
+            Console.Write("Do you want to keep playing? (yes/no) ");
+            keepPlaying = Console.ReadLine();
+        }
     }
 }
