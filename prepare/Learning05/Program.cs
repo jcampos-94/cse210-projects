e2a936e7e3addb02de0c08d26e0b7830a29197e6
jcampos-94 <jcampos.94@hotmail.com>
2023-02-28 16:49:01 -0500

    Learning05 complete

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("red", 3);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("orange", 2, 5);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("yellow", 10);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an are of {area}.");
+        }
     }
 }
